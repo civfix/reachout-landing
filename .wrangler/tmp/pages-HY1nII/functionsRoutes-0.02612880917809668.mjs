c4c7e7d87dbc868330371777a2e6ded50a1a6796
Theo <theo@bongbong.com>
2026-06-09 22:55:47 -0700

    x

---
+++ .wrangler/tmp/pages-HY1nII/functionsRoutes-0.02612880917809668.mjs
@@ -0,0 +1,11 @@
+import { onRequest as ___middleware_js_onRequest } from "C:\\Users\\theob\\GitHub\\reach-out-homepage\\functions\\_middleware.js"
+
+export const routes = [
+    {
+      routePath: "/",
+      mountPath: "/",
+      method: "",
+      middlewares: [___middleware_js_onRequest],
+      modules: [],
+    },
+  ]
